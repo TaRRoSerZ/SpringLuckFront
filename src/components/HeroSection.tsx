788ea8e4e12a52632ac1ce0111316b1b2bc70147
TaRRoS-Dev <kyllianevrard1623@gmail.com>
2025-09-29 16:32:04 +0200

    feat : add a HeroSection component and his style

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import "../styles/HeroSection.css";
+
+export default function HeroSection() {
+	return (
+		<>
+			<section className="hero-section">
+				<h1>Experience the thrill of winning</h1>
+				<img className="main-image" src="/images/main-img.png" />
+			</section>
+		</>
+	);
+}
