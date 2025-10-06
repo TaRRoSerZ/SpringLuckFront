eb15cb031309985e97017f398905e72e1621fb89
TaRRoS-Dev <kyllianevrard1623@gmail.com>
2025-10-06 17:09:14 +0200

    feat : create a BombOrClaat game

--- src/components/GameContainer.tsx
+++ src/components/GameContainer.tsx
@@ -3,23 +3,25 @@ import SloppyBj from "./games/SloppyBj/SloppyBj";
 import DiddySco from "./games/DiddySco/DiddySco";
 import RiggedPaperScissors from "./games/RiggedPaperScissors/RiggedPaperScissors";
 import type { JSX } from "react";
+import BombOrClaat from "./games/BombOrClaat/BombOrClaat";
 
 const GameContainer = () => {
-  const { id } = useParams<{ id: string }>();
+	const { id } = useParams<{ id: string }>();
 
-  const gameMap: Record<string, JSX.Element> = {
-    "2": <SloppyBj />,
-    "3": <DiddySco />,
-    "4": <RiggedPaperScissors />,
-  };
+	const gameMap: Record<string, JSX.Element> = {
+		"1": <BombOrClaat />,
+		"2": <SloppyBj />,
+		"3": <DiddySco />,
+		"4": <RiggedPaperScissors />,
+	};
 
-  const gameComponent = gameMap[id || ""];
+	const gameComponent = gameMap[id || ""];
 
-  if (!gameComponent) {
-    return <div>Jeu non trouvé</div>;
-  }
+	if (!gameComponent) {
+		return <div>Jeu non trouvé</div>;
+	}
 
-  return <div>{gameComponent}</div>;
+	return <div>{gameComponent}</div>;
 };
 
 export default GameContainer;
